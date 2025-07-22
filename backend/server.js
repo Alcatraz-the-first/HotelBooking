96ae0afd8b0b59452bc166c8e976864a02e90c49
GrandmasterGogo <grandmastergogo13@gmail.com>
2025-07-22 23:52:28 +0530

    Updated CORS

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config() ;
 const app = express() ;
 const port = process.env.PORT ;
 app.use(cors({
-  origin: 'http://localhost:3000', // <-- your React app's URL
+  origin: 'https://lads-nt69.onrender.com', // <-- your React app's URL
   credentials: true
 }));
 app.use(express.json()) ;
